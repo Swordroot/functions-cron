f85d5b0286e33a7480ecc56a9f413f1885410ac4
Makoto Tone <tstarouimnpkegto@gmail.com>
2018-10-08 14:22:47 +0900

    微修正

--- functions/src/aggregate.ts
+++ functions/src/aggregate.ts
@@ -1,14 +1,14 @@
-let adminModule = require('./classes/admin.js');
-let admin = adminModule.admin;
+import * as adminModule from './classes/admin.js';
+const admin = adminModule.admin;
 const db = admin.firestore();
-exports.aggregate_rooms_per_user = event => {
+export const aggregate_rooms_per_user = event => {
   console.log();
 }
 
-exports.aggregate_rooms_per_user_avg = event => {
+export const aggregate_rooms_per_user_avg = event => {
   console.log();
 }
 
-exports.aggregate_rooms_count_all = event => {
+export const aggregate_rooms_count_all = event => {
   console.log();
 }
